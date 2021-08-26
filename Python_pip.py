f0ce5697f61c03b09b918d64e5ff4baafa617435
mohowzeh <40325127+mohowzeh@users.noreply.github.com>
2021-08-26 09:25:57 +0200

    Added basic pip explanation

---
+++ Python_pip.py
@@ -0,0 +1,18 @@
+'''
+Basic Python
+
+PIP
+
+(a) freeCodeCamp
+YouTube: https://youtu.be/jBzwzrDvZ18?t=12809
+'''
+
+'''
+PIP installs other Python modules available online
+see https://pypi.org
+
+copy the install command from the module you choose
+paste this command in the Windows prompt (or Mac terminal)
+
+'''
+
