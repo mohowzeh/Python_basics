c42819d52cc2e6bbd8a9c190f9fc3181905ea690
mohowzeh <40325127+mohowzeh@users.noreply.github.com>
2021-08-26 08:58:09 +0200

    Created module1 module2

---
+++ Python_module1.py
@@ -0,0 +1,17 @@
+'''
+Python basic course
+
+MODULES IN PYTHON
+
+(a) freeCodeCamp  
+
+YouTube: https://youtu.be/jBzwzrDvZ18?t=12664
+'''
+
+'''
+Python_module2 calls a function defined in Python_module1
+'''
+
+def say_hi():
+    print('MOdule1 says \'Hi\'.')
+
--- Python_module2.py
+++ Python_module2.py
@@ -8,3 +8,10 @@ MODULES IN PYTHON
 YouTube: https://youtu.be/jBzwzrDvZ18?t=12664
 '''
 
+'''
+Python_module2 calls a function defined in Python_module1
+'''
+
+import Python_module1
+
+Python_module1.say_hi()
